e788c9148db14c2a252408b8c580edef2084d72f
24hr5a3004-collab <24hr5a3004@mtieat.org>
2026-05-24 09:58:05 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "pwa-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "index.html",
+  "style.css",
+  "script.js"
+];
+
+// Install Event
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+    .then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+
+  );
+
+});
+
+// Fetch Event
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+    .then(response => {
+      return response || fetch(event.request);
+    })
+
+  );
+
+});
